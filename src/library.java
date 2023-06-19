85c46e1ede1bc84bd1c69c140c59aac7bd1776ef
ct <wadim_fo@mail.ru>
2023-06-19 23:13:05 +0300

    Добавление библиотеки рандомных чисел

--- src/library.java
+++ src/library.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class library {
+import java.util.ArrayList;
+
+public class library {
+    public static void PrintArray(ArrayList<Integer> arr){
+        for(int element: arr){
+            System.out.printf("%d, ", element);
+        }
+        System.out.println();
+    }
+    public static ArrayList<Integer> FillRandomArray(int n) {
+        ArrayList <Integer> list = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            list.add((int)(Math.random()*200));
+        }
+        return list;
+    }
 }
