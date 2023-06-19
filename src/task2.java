7a818cabe712c61c289df1068519c82449b5fa88
ct <wadim_fo@mail.ru>
2023-06-19 23:13:23 +0300

    Добавление решения задачи №2

--- src/task2.java
+++ src/task2.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class task2 {
+import java.util.ArrayList;
+public class task2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        array = library.FillRandomArray(20);
+        library.PrintArray(array);
+        DeleteEvenNumber(array);
+        library.PrintArray(array);
+    }
+
+    public static void DeleteEvenNumber(ArrayList<Integer> list) {
+        int i = 0;
+        while (i < list.size()) {
+            if (list.get(i) % 2 == 0) list.remove(i);
+            else i++;
+        }
+    }
 }
