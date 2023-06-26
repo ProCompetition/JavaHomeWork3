9f2043c6464918779d9b2ac88cd89a00f200b70d
ct <wadim_fo@mail.ru>
2023-06-27 01:05:27 +0300

    Добавление решения задачи №1 для урока 4

--- src/task4_1.java
+++ src/task4_1.java
@@ -1,25 +1,28 @@
 // Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
 import java.util.LinkedList;
 public class task4_1 {
-    public static LinkedList<Object> fillList(){
-        LinkedList<Object> list = new LinkedList<>();
-        list.add("Start/Finish");
-        list.add("One");
-        list.add("Two");
-        list.add("Three");
-        list.add("Four");
-        list.add("Five");
-        list.add("Six");
-        list.add("Seven");
-        list.add("Finish/Start");
-        return list;
+    public static LinkedList<Object> fillList(){    // создание двусвязного списка
+        LinkedList<Object> month = new LinkedList<>();
+        month.add("January");
+        month.add("February");
+        month.add("March");
+        month.add("April");
+        month.add("May");
+        month.add("June");
+        month.add("July");
+        month.add("August");
+        month.add("September");
+        month.add("October");
+        month.add("November");
+        month.add("December");
+        return month;
     }
-    public static void printList(LinkedList<Object> list){
+    public static void printList(LinkedList<Object> list){ // печать
         for(Object el: list){
             System.out.printf(el + " ");
         }
     }
-    public static void replaceList(LinkedList<Object> list){
+    public static void replaceList(LinkedList<Object> list){ // развертывание списка
         int n = list.size()/2;
         int m = list.size();
         for(int i = 0; i < n; i++){
@@ -28,7 +31,7 @@ public class task4_1 {
             list.set(m-i-1, temp);
         }
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) { // вывод
         LinkedList<Object> listFirst = fillList();
         printList(listFirst);
         replaceList(listFirst);
